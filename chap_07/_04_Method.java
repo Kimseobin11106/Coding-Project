290c147b34c7c7add85a242b655b37f35b2595bd
Kimseobin <128437643+Kimseobin11106@users.noreply.github.com>
2023-06-20 17:36:15 +0900

    _04_Method.java

--- chap_07/_04_Method.java
+++ chap_07/_04_Method.java
@@ -8,12 +8,19 @@ public class _04_Method {
 		
 		b1.autoReport();
 		BlackBox.canAutoReport = true;
+		// 자동 충돌 감지 업데이트 완료
 		b1.autoReport();
 		
 		b1.insertMemoryCard(512);
-		// 자동 충돌 감지 업데이트 완료
-		BlackBox.canAutoReport = true;
-		
+
+		// 일반 영상 : 1 (type)
+		// 이벤트 영상(충돌 감지) : 2
+
+		int fileCount = b1.getVideoFileCount(1); // 일반 영상
+		System.out.println("일반 영상 파일 수 : " + fileCount + "개");
+
+		fileCount = b1.getVideoFileCount(2); // 이벤트 영상
+		System.out.println("이벤트 영상 파일 수 : " + fileCount + "개");
 	}
 
 }
